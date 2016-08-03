e62cb160a5c026828378716f4662b295d7165547
Jan Moeller <jan.moeller0@gmail.com>
2016-08-03 22:13:49 +0200

    Add install alias function

--- antergos.sh
+++ antergos.sh
@@ -1,31 +1,36 @@
 sudo -v
 
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-yaourt -S --noconfirm pacaur
-pacaur -Syu
+yaourt -Sy --noconfirm pacaur
 
-pacaur -S --noconfirm xorg-xinit xorg-server xorg-utils xorg-server-utils
+install() {
+    /bin/pacaur -S --noconfirm "$@"
+}
+
+install "-yu"
+
+install xorg-xinit xorg-server xorg-utils xorg-server-utils
 # replace i3status with i3block
-pacaur -S --noconfirm i3-wm i3status i3lock
+install i3-wm i3status i3lock
 
 # replace with outer launcher
-pacaur -S --noconfirm dmenu
+install dmenu
 
 mkdir ~/.i3
 cp /etc/i3/config ~/.i3/
 echo "exec i3" >> ~/.xinitrc
 
 # x terminal simulator, alternative for xterm
-pacaur -S --noconfirm rxvt-unicode
-pacaur -S --noconfirm zsh
+install  rxvt-unicode
+install  zsh
 echo "[[ -f ~/.zshrc ]] && ~/.zshrc" >> ~/.zprofile
 echo "[ -z \"$DISPLAY\" -a \"$(fgconsole)\" -eq 1 ] && exec startx" >> ~/.zprofile
 
 # oh my zsh
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o zshohmy.sh
-sed -i -e "s/env zsh//" zshohmy.sh
-sh zshohmy.sh
-rm zshohmy.sh
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o ~/zshohmy.sh
+sed -i -e "s/env zsh//" ~/zshohmy.sh
+sh ~/zshohmy.sh
+rm ~/zshohmy.sh
 
 sudo sed -i -e 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
 sudo sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
