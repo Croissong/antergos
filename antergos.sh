0c352da774fca322a9bf03ee2ed75699af6fc938
Semih Helvaci <semih.helvaci92@gmail.com>
2016-08-03 20:49:10 +0200

    Pacaur

--- antergos.sh
+++ antergos.sh
@@ -1,21 +1,25 @@
-sudo pacman -Syu
-sudo pacman -S --noconfirm xorg-xinit xorg-server xorg-utils xorg-server-utils
+sudo -v
+
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+yaourt -S --noconfirm pacaur
+sudo pacaur -Syu
+
+sudo pacaur -S --noconfirm xorg-xinit xorg-server xorg-utils xorg-server-utils
 # replace i3status with i3block
-sudo pacman -S --noconfirm i3-wm i3status i3lock
+sudo pacaur -S --noconfirm i3-wm i3status i3lock
 
 # replace with outer launcher
-sudo pacman -S --noconfirm dmenu
+sudo pacaur -S --noconfirm dmenu
 
 mkdir ~/.i3
 cp /etc/i3/config ~/.i3/
 echo "exec i3" >> ~/.xinitrc
 
 # x terminal simulator, alternative for xterm
-sudo pacman -S --noconfirm rxvt-unicode
-sudo pacman -S --noconfirm zsh
+sudo pacaur -S --noconfirm rxvt-unicode
+sudo pacaur -S --noconfirm zsh
 echo "[[ -f ~/.zshrc ]] && ~/.zshrc" >> ~/.zprofile
 echo "[ -z \"$DISPLAY\" -a \"$(fgconsole)\" -eq 1 ] && exec startx" >> ~/.zprofile
-chsh -s /bin/zsh
 
 # oh my zsh
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o zshohmy.sh
@@ -27,23 +31,8 @@ sudo sed -i -e 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
 sudo sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sudo locale-gen
 
-localectl set-locale LANG=en_US.UTF-8
-# sudo echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-# sudo echo "LANGUAGE=en_US.UTF-8:en" >> /etc/locale.conf
-# sudo echo "LC_ADDRESS=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_COLLATE=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_CTYPE=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_IDENTIFICATION=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_MEASUREMENT=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_MESSAGES=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_MONETARY=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_NAME=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_NUMERIC=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_PAPER=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_TELEPHONE=de_DE.UTF-8" >> /etc/locale.conf
-# sudo echo "LC_TIME=de_DE.UTF-8" >> /etc/locale.conf
+sudo localectl set-locale LANG=en_US.UTF-8
 
-#Keyboard layout
-localectl --no-convert set-x11-keymap de
+sudo localectl --no-convert set-x11-keymap de
 
 #sudo pacman -S --noconfirm emacs
