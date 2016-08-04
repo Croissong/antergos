f27d325b375afc35f4fe8e4044e80a2a417b6cab
Jan Moeller <jan.moeller0@gmail.com>
2016-08-04 19:32:05 +0200

    merge sh file into org file

--- antergos.sh
+++ antergos.sh
@@ -2,19 +2,14 @@ sudo -v
 
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
 yaourt -Sy --noconfirm pacaur
-
 install() {
     /bin/pacaur -S --noconfirm "$@"
 }
-
 install "-yu"
 
 install xorg-xinit xorg-server xorg-utils xorg-server-utils
 # replace i3status with i3block
 install i3-wm i3status i3lock
-
-# replace with outer launcher
-install dmenu
 
 mkdir ~/.i3
 cp /etc/i3/config ~/.i3/
@@ -23,6 +18,7 @@ echo "exec i3" >> ~/.xinitrc
 
 # x terminal emulator
 install termite
+
 install zsh
 echo "[[ -f ~/.zshrc ]] && ~/.zshrc" >> ~/.zprofile
 echo "[ -z \"$DISPLAY\" -a \"$(fgconsole)\" -eq 1 ] && exec startx" >> ~/.zprofile
@@ -33,6 +29,10 @@ curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools
 sed -i -e "s/env zsh//" ~/zshohmy.sh
 sh ~/zshohmy.sh
 rm ~/zshohmy.sh
+
+# Install+enable zsh theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel9k\/powerlevel9k" POWERLEVEL9K_MODE="awesome-fontconfig"/' ~/.zshrc
 
 #Install fonts
 mkdir -p ~/.local/share/fonts
@@ -48,17 +48,8 @@ curl -fsSL https://github.com/powerline/powerline/raw/develop/font/10-powerline-
 
 fc-cache -fv
 
-
-# Install+enable zsh theme
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel9k\/powerlevel9k" POWERLEVEL9K_MODE="awesome-fontconfig"/' ~/.zshrc
-
 sudo sed -i -e 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
 sudo sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sudo locale-gen
-
 sudo localectl set-locale LANG=en_US.UTF-8
-
 sudo localectl --no-convert set-x11-keymap de
-
-#pacaur -S --noconfirm emacs
