74bb5b3956e1da1220c434d81669c389d5937720
Jan Möller <jan.moeller0@gmail.com>
2017-05-31 14:48:44 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 curl https://raw.githubusercontent.com/Croissong/.dotfiles/master/keys.map | loadkeys
 timedatectl set-ntp true
+sgdisk -o \
+-n 0:0:+1G -t 0:ef00 -c 0:”efi /boot” \
+-n 0:0:+30G -t 0:8300 -c 0:“/” \
+-n 0:0:0 -t 0:8300 -c 0:”/home” \
+/dev/sda
