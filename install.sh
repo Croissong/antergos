435b02d1a32d96dd9b005e8505dd326d7fd91afe
Jan Möller <jan.moeller0@gmail.com>
2017-06-04 19:04:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ loadkeys my-keys
  
 timedatectl set-ntp true
 sgdisk -o \
--n 0:0:+1G -t 0:ef00 -c 0:”efi /boot” \
--n 0:0:+30G -t 0:8300 -c 0:“/” \
--n 0:0:0 -t 0:8300 -c 0:”/home” \
+-n 0:0:+1G -t 0:ef00 -c 0:"efi /boot" \
+-n 0:0:+30G -t 0:8300 -c 0:"/" \
+-n 0:0:0 -t 0:8300 -c 0:"/home" \
 /dev/sda
  
 mkfs.vfat /dev/sda1
