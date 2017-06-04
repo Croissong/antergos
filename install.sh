51ab97ae59890d914272fb579c4c205585667de5
Jan Möller <jan.moeller0@gmail.com>
2017-06-04 17:25:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 read -p “username: “ username
 read -sp “password: “ password
 read -p “hostname: “ hostname
- 
+
+alias install="pacman -S --noconfirm"
+
 github=https://raw.githubusercontent.com/Croissong
 curl $github/.dotfiles/master/my-keys.map > /usr/share/kbd/keymaps/my-keys.map 
 loadkeys my-keys
@@ -23,10 +25,11 @@ mkdir /mnt/home
 mount /dev/sda3 /mnt/home
  
 mount -o remount,size=2G /run/archiso/cowspace
-pacman -S reflector
+install reflector
 reflector --latest 10 --age 24 --protocol https  --sort rate --save /etc/pacman.d/mirrorlist
  
 curl $github/arch/master/etc/locale.gen > /etc/locale.gen
+curl $github/arch/master/etc/sudoers > /etc/sudoers
  
 pacstrap /mnt base
  
@@ -42,19 +45,26 @@ echo $hostname > /etc/hostname
 sed -i ‘/::1/a 127.0.1.1\t$hostname.localdomain\t$hostname’ /etc/hosts
 echo root:$password | chpasswd
  
-pacman -S  intel-ucode linux-lts linux-lts-headers
+install intel-ucode linux-lts linux-lts-headers
 bootctl --path=/boot install
 curl $github/arch/master/boot/loader.conf > /boot/loader/loader.conf
 curl $github/arch/master/boot/arch.conf > /boot/loader/entries/arch.conf
 curl $github/arch/master/boot/arch-lts.conf > /boot/loader/entries/arch-lts.conf
 
-pacman -S connman
+install connman
 systemctl enable connman
 
-pacman -S zsh
+install zsh sudo
 useradd -m -G wheel -s /bin/zsh $username
 echo $username:$password | chpasswd 
- 
+
+install --needed binutils make gcc fakeroot expac yajl git pkg-config
+
+su skender && cd ~
+arch=https://aur.archlinux.org
+git clone $aur/cower.git && cd cower && makepkg -i --skippgpcheck --needed && cd - && rm -rf cower
+git clone $aur/pacaur.git && cd pacaur && makepkg -i --needed && cd - && rm -rf pacaur
+
 exit
 umount -R /mnt
 reboot
